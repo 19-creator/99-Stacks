c5b4f62696b120803164eaa4cc82c15224ce1927
19 <84217661+19-creator@users.noreply.github.com>
2024-07-20 13:59:05 -0500

    Fix container mixin not loading in production

--- src/main/java/com/nineteen/xcixstacks/mixin/ContainerMixin.java
+++ src/main/java/com/nineteen/xcixstacks/mixin/ContainerMixin.java
@@ -1,10 +1,17 @@
 package com.nineteen.xcixstacks.mixin;
 
+import net.minecraft.world.Container;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @SuppressWarnings({"unused", "MissingUnique", "AddedMixinMembersNamePattern"})
-@Mixin(targets = "net.minecraft.world.Container")
+@Mixin(Container.class)
 public interface ContainerMixin {
+    /**
+     * @author Nineteen
+     * @reason Can't inject into interface method.
+     */
+    @Overwrite
     default int getMaxStackSize() {
         return 99;
     }
